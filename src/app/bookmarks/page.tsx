29bd61812912da1af952f03b378965e818f7cc47
mysoyul <vega2k_@naver.com>
2025-02-20 16:54:23 +0900

    Pagination 컴포넌트 추가

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -15,6 +15,7 @@ export default async function BookmarkPage(props: Props)  {
 
   return (
     <div>
+        <h1>Welcome to Bookmarker</h1>
         <Bookmarks bookmarks={bookmarksRes} />
     </div>
   );
--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,6 +1,7 @@
 import { BookmarksResponse } from '@/types';
 import React from 'react';
-import Bookmark from './Bookmark';
+import Bookmark from '@/app/components/Bookmark';
+import Pagination from '@/app/components/Pagination';
 
 type Props = {
     bookmarks: BookmarksResponse
@@ -9,10 +10,11 @@ type Props = {
 const Bookmarks: React.FC<Props> = ({bookmarks}) => {
     return (
         <div>
-            <h1>Welcome to Bookmarker</h1>
+            <Pagination bookmarks={bookmarks} />
             {
-            bookmarks.data.map(bookmark => 
-                <Bookmark key={bookmark.id} bookmark={bookmark} />)
+                bookmarks.data.map(bookmark => 
+                    <Bookmark key={bookmark.id} bookmark={bookmark} />
+                )
             }            
         </div>
     );
